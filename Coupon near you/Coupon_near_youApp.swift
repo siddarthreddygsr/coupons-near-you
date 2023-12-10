8a6663bfbe224cb240d46ea50e152372b1e2eb9e
siddarthreddygsr <siddarthreddygsr@gmail.com>
2023-12-10 21:47:26 +0530

    Initial Commit

---
+++ Coupon near you/Coupon_near_youApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coupon_near_youApp.swift
+//  Coupon near you
+//
+//  Created by Siddarth Reddy on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Coupon_near_youApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coupon near youUITests/Coupon_near_youUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Coupon_near_youUITestsLaunchTests.swift
+//  Coupon near youUITests
+//
+//  Created by Siddarth Reddy on 10/12/23.
+//
+
+import XCTest
+
+final class Coupon_near_youUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
